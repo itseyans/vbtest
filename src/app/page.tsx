e35f0c5673d089acdf0129fa5f10ecec1468b482
BryanAnthony08 <xxwolfangxx.bryan@gmail.com>
2025-03-07 15:15:01 +0800

    Ad info / Listed Veh View
    Added additional info  js panel
    Added Listed Vehicle Viewing JS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import RegistrationForm from "../vcomp/RegistrationForm";
+import SignButton from "../vcomp/SignButton";
+
 export default function Home() {
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20">
       <main className="flex flex-col gap-8 items-center">
-        <h1 className="text-2xl font-bold">Welcome to My Website</h1>
+        <h1 className="text-2xl font-bold">what</h1>
       </main>
     </div>
   );
